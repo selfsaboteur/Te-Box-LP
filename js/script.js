5b6e05ed8591ca9d61392f56db4fc743e2c3d824
David Khabulia <31065900+selfsaboteur@users.noreply.github.com>
2023-01-11 18:22:12 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("btn");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-red", "");
+    }
+    x[slideIndex - 1].style.display = "grid";
+}
